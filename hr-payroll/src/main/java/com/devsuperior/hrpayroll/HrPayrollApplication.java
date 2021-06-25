84a68b24a35b25f38babb983c710158a01e78b08
Rodrigo Lúcio <56269484+rodrigo-lucio@users.noreply.github.com>
2021-06-24 22:13:24 -0300

    Configurado Microsserviços clientes do Eureka

--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/HrPayrollApplication.java
@@ -2,10 +2,12 @@ package com.devsuperior.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@RibbonClient(name = "hr-worker")
+@EnableEurekaClient
+//@RibbonClient(name = "hr-worker") Nao precisa mais pois o eureka ja tem o balanceador de carga
 @EnableFeignClients
 @SpringBootApplication
 public class HrPayrollApplication {
