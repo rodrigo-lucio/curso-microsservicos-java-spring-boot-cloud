24b47e6db3f198ed55b6ea6a4c49a92b3e11ec95
Rodrigo Lúcio <56269484+rodrigo-lucio@users.noreply.github.com>
2021-06-30 22:24:22 -0300

    Criado projeto do config server

--- hr-api-gateway-zuul/src/main/java/com/devsuperior/hrapigatewayzuul/HrApiGatewayZuulApplication.java
+++ hr-api-gateway-zuul/src/main/java/com/devsuperior/hrapigatewayzuul/HrApiGatewayZuulApplication.java
@@ -1,4 +1,4 @@
-package com.devsuperior.hreapigatewayzuul;
+package com.devsuperior.hrapigatewayzuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
