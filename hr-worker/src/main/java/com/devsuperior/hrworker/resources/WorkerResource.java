12b787cd3348e078ce93cc2fead82d37f3082d06
Rodrigo Lúcio <56269484+rodrigo-lucio@users.noreply.github.com>
2021-07-14 23:24:49 -0300

    Configurações de banco de dados no serviço de configuração

--- hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
@@ -50,13 +50,13 @@ public class WorkerResource {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id) {
 
-// Caso queira testar a tolererancia a falha do hystrix ou do zuul
-		try {
-			Thread.sleep(3000L);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+// Caso queira testar a tolererancia a falha do hystrix ou do zuul por TIMEOUT
+//		try {
+//			Thread.sleep(3000L);
+//		} catch (InterruptedException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
 //		
 		logger.info("PORT WORKER=" + env.getProperty("local.server.port"));
 
